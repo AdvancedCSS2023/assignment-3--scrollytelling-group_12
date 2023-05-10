f2985a907f889c7ad74fea95f3834d51f555bc70
Tobias Fjellvang <tobiasfjell2001@gmail.com>
2023-05-10 13:16:31 +0200

    fixed scroll

--- script.js
+++ script.js
@@ -2,23 +2,21 @@
 gsap.registerPlugin(ScrollTrigger);
 
 gsap.to(".scene2_ground", {
-duration: 8,
-scrollTrigger: {
-trigger: ".scene3",
-start: "top",
-end: "bottom",
-scrub: 4,
-toggleActions: "restart none none none",
-//Pin verdier: true, false
-pin: ".scene2_ground",
-//  Hvis man velger et element til pin, så er det lurt å legge til pinSpacing
-      //I dette eksemplet lander square1 på square2
-      // false = gjør ingenting, true = legger til auto padding, 
-pinSpacing: true,
-markers: {
-startColor: "purple",
-endColor: "fuchsia",
-fontSize: "3rem",
-},
-},
-});
+      duration: 8,
+      scrollTrigger: {
+        trigger: ".scene2_ground",
+        start: "bottom bottom",
+        endTrigger: ".scene2", // change to end at the end of scene2
+        end: "bottom bottom",
+        scrub: 1,
+        toggleActions: "restart none none none",
+        pin: ".scene2_ground",
+        pinSpacing: true,
+        markers: {
+          startColor: "purple",
+          endColor: "fuchsia",
+          fontSize: "3rem",
+        },
+      },
+    });
+    
