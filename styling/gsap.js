4936ffcb5bed158aca0a89ddfb65cdfa81262347
PetterFuruli <pettersf@stud.ntnu.no>
2023-05-10 12:06:36 +0200

    plugins

--- styling/gsap.js
+++ styling/gsap.js
@@ -1,207 +1,331 @@
-// Aktivere scrolltrigger
-gsap.registerPlugin(ScrollTrigger);
-
-// Husk å link pluginsene i HTML
-
-//<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.5/gsap.min.js"></script>
-//<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.5/ScrollTrigger.min.js"></script>
-
-// Scrolltrigger = Objekt X aktiveres når objekt X/Y dukker opp i skjermen
-
-gsap.to(".square", {
-  x: 700,
-  duration: 3,
-  scrollTrigger: ".square2",
-});
-
-// Designe Markers (Ikke viktig)
-
-markers: {
-  startColor;
-  "purple", endColor;
-  "fuchsia", fontSize;
-  "4rem", indent;
-  200;
-}
-
-// Lage toggleClass endrer på fargen til objektet
-
-gsap.to(".square", {
-  duration: 3,
-  scrollTrigger: {
-    trigger: ".square",
-    start: "top 30%",
-    end: "center 20%",
-    // Bedre metode: legger til verdien av .Square (høyden) som den er (mer dynamisk) Her kan man legge til vh! (i css)
-    end: () => `+=${document.querySelector(".square").offsetHeight}`,
-    markers: true,
-    // Ny animasjon lagt til i CSS
-    toggleClass: "red",
-  },
-});
-
-// 3
-// Toogle Actions
-
-gsap.to(".square", {
-  x: 1000,
-  duration: 8,
-  scrollTrigger: {
-    trigger: ".square",
-    start: "top 60%",
-    end: "top 40%",
-    //Legger til Toogle Actions med en string (Play, pause, resuem, reverse, restart, reset, complete, none)
-    toggleActions: "restart pause resume complete",
-    onEnter,
-    onLeave,
-    onEnterBack,
-    onLeaveBack,
-    markers: {
-      startColor: "purple",
-      endColor: "fuchsia",
-      fontSize: "3rem",
-    },
-  },
-});
-
-// 4
-// Scrub Property = Kontrollere animasjonen med scroll! NB! VIKTIG!
-
-gsap.to(".square", {
-  x: 1000,
-  duration: 8,
-  scrollTrigger: {
-    trigger: ".square",
-    start: "top 80%",
-    end: "top 30%",
-    // Her aktiverer man Scrub: Man kan endre verdien også slik at animasjonen blir mer/mindre smooth: (true, x-sekunder)
-    scrub: true,
-    toggleActions: "restart pause resume complete",
-    markers: {
-      startColor: "purple",
-      endColor: "fuchsia",
-      fontSize: "3rem",
-    },
-  },
-});
-
-// 5 Pinning!
-
-gsap.to(".square", {
-  duration: 8,
-  scrollTrigger: {
-    trigger: ".square2",
-    start: "top 80%",
-    end: "top 30%",
-    scrub: 4,
-    //     toggleActions: "restart none none none",
-    // Pin verdier: true, false
-    pin: ".square",
-    // Hvis man velger et element til pin, så er det lurt å legge til pinSpacing
-    // I dette eksemplet lander square1 på square2
-    //false = gjør ingenting, true = legger til auto padding,
-    pinSpacing: true,
-    markers: {
-      startColor: "purple",
-      endColor: "fuchsia",
-      fontSize: "3rem",
-    },
-  },
-});
-
 // 6
 // Timeline
 
 gsap.registerPlugin(ScrollTrigger);
 
-//Aktivere animasjon, men blir et problem hvis deleyet overskrider hverandre
-gsap.to(".box", { x: 500, duration: 5 });
-gsap.to(".box", { y: 200, duration: 3, delay: 2 });
-gsap.to(".box", { y: 0, duration: 2, delay: 5 });
+
+/* Aktivere animasjon, men blir et problem hvis deleyet overskrider hverandre
+gsap.to(".box", {x: 500, duration: 5})
+gsap.to(".box", {y: 200, duration: 3, delay: 2})
+gsap.to(".box", {y: 0, duration: 2, delay: 5 })
+*/
 
 // LØSNING:
 //Timeline:
 
-const tl = gsap.timeline();
 
-tl.to(".box", { x: 500, duration: 2 });
-to(".box", { y: 200, duration: 3 });
-to(".box", { x: 0, duration: 2 });
+
+
 
 // Legge til ScrollTrigger
 
 const tl = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".box",
-    markers: true,
-    start: "top 80%",
-    end: "top 30%",
-    scrub: 1,
-  },
+    scrollTrigger: {
+        trigger: ".box",
+        start: "top 90%",
+        end: "top 30%",
+        scrub: 1
+    }
 });
 
-tl.to(".box", { x: 500, duration: 5 })
-  .to(".box", { y: 200, duration: 2 })
-  .to(".box", { x: 0, duration: 2 });
+tl.to(".box", {x: 500, duration: 5})
+.to(".box", {y: 200, duration: 2})
+.to(".box", {x: 0, duration: 2})
+
+
+
+
+/*
+const tl2 = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#movieboard",
+        markers: true,
+        start: "top 50%",
+        end: "top 30%",
+        scrub: 1
+    }
+});
+
+tl2.to("#movieboard", {x: 500, duration: 5})
+.to("#movieboard", {y: 200, duration: 2})
+.to("#movieboard", {x: 0, duration: 2})
+*/
+
+
+
+
+
+/* animasjon som tar objektet utenfor skjermen og plasseren den i midten. */
+/*
+TweenMax.set("#movieboard", {xPercent:-50, yPercent:-50, left:"50%",y:-100})
+
+var h = window.innerHeight,
+    tl3 = new TimelineMax({yoyo:true, repeat: 0, repeatDelay:0.3});
+
+tl3.to("#movieboard", 0.5, {y:h})
+*/
+
+
+
+
 
 // 7
 // Standalone ScrollTriggers = Triggre Classes når man scroller ned
 
+/*
 ScrollTrigger.create({
-  trigger: ".box",
-  start: "top 80%",
-  end: "top 50%",
-  markers: true,
-  toggleClass: "box-red",
-});
+    trigger: ".box",
+    start: "top 80%",
+    end: "top 50%",
+    markers: true,
+    toggleClass: "box-red"
+})
+*/
 
+/*
 // Endrer på nav bar med Scroll trigger. Blir aktivert etter top 6%
 ScrollTrigger.create({
   markers: true,
-  start: "top 6%",
-  trigger: ".panel",
-  toggleClass: { targets: "nav", className: "nav-active" },
+    start: "top 6%",
+    trigger: ".panel",
+    toggleClass: { targets: "nav", className: "nav-active" },
 });
 
-// Endrer class med toggle actions og rekkefølger. Bytt console.log med hvilke som helst event du vil!
+*/
+
+// Endrer class med toggle actions og rekkefølger. Bytt console.log med hvilke som helst event du vil! 
 // Oversikt over alle ScrollTrigger Events: https://greensock.com/docs/v3/Plugins/ScrollTrigger
 
+/*
 ScrollTrigger.create({
-  markers: true,
-  start: "top 80%",
-  end: "top 50%",
-  trigger: ".box",
-  //EXempel på events:
-  onUpdate: (self) => console.log(self),
-  onEnter: () => console.log("enter!"),
-  onLeave: () => console.log("leave!"),
-  OnEnterBack: () => console.log("enterBack!"),
-  onLeaveBack: () => console.log("leaveBack!"),
+    markers: true,
+    start: "top 80%",
+    end: "top 50%",
+    trigger: ".box",
+    //EXempel på events:
+    onUpdate: (self) => console.log(self),
+    onEnter: () => console.log("enter!"),
+    onLeave: () => console.log("leave!"),
+    OnEnterBack: () => console.log("enterBack!"),
+    onLeaveBack: () => console.log("leaveBack!")
 });
+*/
 
-gsap.to("#scene1", {
-  background:
-    "linear-gradient(to bottom, #a8c0ff 0%, #FF5733 10%, #1b1c20 20%, #FFA500 30%, #a8c0ff 40%, #FF5733 50%, #1b1c20 60%, #FFA500 70%, #FF5733 80%, #1b1c20 90%, #FFA500 100%",
 
-  duration: 3,
 
+
+
+
+
+// 5 Pinning!
+
+ // gsap.to("#movieboard", {
+ //   duration: 8,
+ //   scrollTrigger: {
+ //     trigger: ".square2",
+ //     start: "top 80%",
+ //   end: "top 30%",
+ // scrub: 4,
+ //     toggleActions: "restart none none none",
+      // Pin verdier: true, false
+ //     pin: ".square",
+      // Hvis man velger et element til pin, så er det lurt å legge til pinSpacing
+      // I dette eksemplet lander square1 på square2
+      // false = gjør ingenting, true = legger til auto padding, 
+ //     pinSpacing: true,
+ //    markers: {
+ //       startColor: "purple",
+ //    endColor: "fuchsia",
+ //    fontSize: "3rem",
+ //   },
+ //   },
+ // });
+
+ ScrollTrigger.create ({
+    trigger:"#space",
+    start: "top 20%",
+    end: "bottom 80%",
+    pin:"#movieboard"
+ })
+
+
+
+// 4
+// Scrub Property = Kontrollere animasjonen med scroll! NB! VIKTIG!
+
+gsap.to("#movieboard", {
+  x: -1500,
+ duration: 5,
   scrollTrigger: {
-    trigger: "body",
+  trigger: "#space",
+  start: "bottom 70%",
+   end: "bottom bottom",
 
-    start: "top top",
+    scrub: 3,
+   toggleActions: "none none none none",
 
+ },
+ });
+
+
+
+
+ ScrollTrigger.create ({
+    trigger:"#space",
+    start: "top",
+    end: "bottom 80%",
+    pin:".curtains"
+ })
+
+
+ // Scrub Property = Kontrollere animasjonen med scroll! NB! VIKTIG!
+
+
+
+ const tl_left_curtain = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#space",
+        start: "top",
+        end: "bottom bottom",
+        scrub: 1
+    }
+});
+
+tl_left_curtain.to(".left-curtain", {xPercent: -100, duration: 1})
+.to(".box", {y: 200, duration: 1})
+.to(".box", {x: 0, duration: 1})
+
+
+const tl_Right_curtain = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#space",
+        start: "top",
+        end: "bottom bottom",
+        scrub: 1
+    }
+});
+
+tl_Right_curtain.to(".right-curtain", {xPercent: 100, duration: 1})
+.to(".box", {y: 200, duration: 1})
+.to(".box", {x: 0, duration: 1})
+
+
+
+
+
+gsap.utils.toArray("span").forEach((span) => {
+    ScrollTrigger.create({
+        trigger: "#space",
+        start: "top",
+        toggleClass: "active",
+        /*Eller den forblir*/
+        onEnter: () => span.classList.add("active"),
+    
+    })
+});
+
+
+
+
+
+gsap.to("#birds1", {
+    x: 2000,
+    y: -500,
+   duration: 5,
+    scrollTrigger: {
+    trigger: "#space",
+    start: "bottom 90%",
+     end: "top 60%",
+      scrub: 5,
+    
+   },
+   });
+
+
+   gsap.to("#astroid", {
+    x: 5000,
+    y: 4000,
+   duration: 5,
+    scrollTrigger: {
+    trigger: "#satelite",
+    start: "bottom",
+     end: "bottom",
+     
+     
+    
+    
+   },
+   });
+
+
+
+
+   gsap.to("#scene1", {
+    background: "linear-gradient(to bottom, #a8c0ff 0%, #FF5733 10%, #1b1c20 30%, #FFA500 40%, #a8c0ff 50%, #FF5733 60%, #1b1c20 80%, #FFA500 90%, #FF5733 100%",
+    duration: 3,
+    scrollTrigger: {
+      trigger: "body",
+      start: "top top",
+      end: "bottom bottom",
+      scrub: 1
+    }
+  });
+
+
+  /*
+   background: "linear-gradient(to bottom, #a8c0ff 0%, #FF5733 10%, #1b1c20 20%, #FFA500 30%, #a8c0ff 40%, #FF5733 50%, #1b1c20 60%, #FFA500 70%, #FF5733 80%, #1b1c20 90%, #FFA500 100%",
+   */
+
+  ScrollTrigger.create ({
+    trigger:"#scene1",
+    start: "top",
     end: "bottom bottom",
+    pin:".ground"
+ })
 
-    scrub: 1,
-  },
-});
+ 
 
-ScrollTrigger.create({
-  trigger: "#scene1",
+/*
+   gsap.utils.toArray(".color").forEach(function(elem) {
 
-  start: "top top",
+    var color = elem.getAttribute('data-color');
+    
+    ScrollTrigger.create({
+      trigger: elem,
+      start:'top',
+      end:'bottom',
+      onEnter: () => gsap.to('#scene1', {backgroundColor:color}), 
+      onLeave: () => gsap.to('#scene1', {backgroundColor:'white'}), 
+      onLeaveBack: () => gsap.to('#scene1', {backgroundColor:'white'}), 
+      onEnterBack: () => gsap.to('#scene1', {backgroundColor:color}), 
+      markers:true
+    });
+  
+  });
+*/
 
-  end: "bottom bottom",
 
-  pin: ".ground",
-});
+
+  // 2
+// Lage toggleClass endrer på fargen til objektet
+
+// gsap.to(".square", {
+//  duration: 3,
+// scrollTrigger: {
+//   trigger: ".square",
+// start: "top 30%",
+// end: "center 20%",
+// Bedre metode: legger til verdien av .Square (høyden) som den er (mer dynamisk) Her kan man legge til vh! (i css)
+// end: () => `+=${document.querySelector(".square").offsetHeight}`,
+// markers: true,
+// Ny animasjon lagt til i CSS
+// toggleClass: "red"
+// }
+// })
+
+
+
+  
+ 
+  
+ 
