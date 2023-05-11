bdeca32e77cb07a43a2040f387173f44d35465cd
Tobias Fjellvang <tobiasfjell2001@gmail.com>
2023-05-11 17:10:15 +0200

    javascript scene 2 and 4

--- script.js
+++ script.js
@@ -344,3 +344,63 @@ tlSun.to(".sun", {x:1050, y:-850, duration: 4})
 .to(".sun", {x: 2500, y: 0, duration: 4})
 
 
+//scroll scene2
+gsap.to(".scene2_ground", {
+  duration: 8,
+  scrollTrigger: {
+    trigger: ".scene2_ground",
+    start: "bottom bottom",
+    endTrigger: ".scene2", // change to end at the end of scene2
+    end: "bottom bottom",
+    scrub: 1,
+    toggleActions: "restart none none none",
+    pin: ".scene2_ground",
+    pinSpacing: true,
+  },
+});
+
+//scroll scene4
+gsap.to(".scene4_ground", {
+duration: 8,
+scrollTrigger: {
+trigger: ".scene4_ground",
+start: "bottom bottom",
+endTrigger: ".scene4", // change to end at the end of scene2
+end: "bottom bottom",
+scrub: 1,
+toggleActions: "restart none none none",
+pin: ".scene4_ground",
+pinSpacing: true,
+},
+});
+
+//airplane takeoff
+const airplane = gsap.timeline({
+scrollTrigger: {
+trigger: ".scene2_ground",
+start: "top 70%",
+end: "top 30%",
+scrub: 1
+}
+});
+
+airplane.to("#scene2_airplane", {x: 250, y: -45, duration: 2})
+    .to("#scene2_airplane", {x: 1000, y: -500, duration: 5})
+    .to(".scene2_ground", {opacity: 0, duration: 1}, "-=1");
+
+
+
+//airplane landing
+const landing = gsap.timeline({
+scrollTrigger: {
+trigger: ".scene4_ground",
+start: "top 70%",
+end: "top 30",
+scrub: 1
+}
+});
+    
+landing.to("#scene4_airplane", {x: -750, y: 450, duration: 2})
+.to("#scene4_airplane", {x: -1100, y: 520, duration: 5});
+    
+   
